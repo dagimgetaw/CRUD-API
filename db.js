c46a1100c779840182c62b523e638abd071fb370
Dagim <dagimgetaW27@gmail.com>
2025-03-02 22:03:06 +0300

    fix the db name

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-const name = process.env.DB_USERNAME;
+const name = process.env.DB_NAME;
 const url = `mongodb+srv://${username}:${password}@backenddb.k4asq.mongodb.net/?retryWrites=true&w=majority&appName=${name}`;
 
 const connectDB = async () => {
